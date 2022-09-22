dcae45194bc954385058c73de96e2ac2e600e4e9
fataneh <fkarandish1@gmail.com>
2022-09-21 22:37:39 -0700

    raise valueerror

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -6,7 +6,7 @@ from average_calculator.calc import rounded_average
 def test_average_of_two_numbers_is_properly_computed():
     actual = rounded_average([4, 6])
     # Delete the skip and write the proper assertion here!
-    pass
+    assert actual == 2
 
 @pytest.mark.skip
 def test_average_of_empty_list_raises_ValueError():
